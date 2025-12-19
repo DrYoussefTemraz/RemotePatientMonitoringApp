93e580a9f1fe6d6b6d8d08d4ad737016fdb8e65e
DrYoussefTemraz <youseftahertemraz@gmail.com>
2025-12-19 21:24:57 +0200

    fix the container class to the app-container and fix the home page sub-container

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import PatientForm from "@/components/forms/PatientForm";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
-      <section className="remove-scrollbar container my-auto">
-        <div className="subcontainer max-w-[496px]">
+      <section className="remove-scrollbar app-container my-auto">
+        <div className="sub-container max-w-[496px]">
           <Image
             src='/assets/icons/logo-full.svg'
             alt="patient"
@@ -12,12 +13,11 @@ export default function Home() {
             height={1000}
             className="mb-12 h-10 w-fit"
           />
-
-
-
+          <PatientForm />
         </div>
 
       </section>
+      
 
     </div>
   );
